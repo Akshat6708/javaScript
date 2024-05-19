964f5710299fac6c3f7c4430aa067349616c4e47
Nitin bansiya <akshatpatidar29@gmail.com>
2024-05-19 11:47:37 +0530

    add day_2 folder files

---
+++ conditional.js
@@ -0,0 +1,56 @@
+1// let age=20;
+// if(age>=18){
+//     console.log("can vote");
+
+// }
+// else{
+//     console.log("cannot vote")
+// }
+
+// let number=6;
+// if(number==1){
+//     console.log("jai hoo");
+// }
+// else if(number==2){
+//     console.log("maja aa gya");
+// }
+// else if(number==3){
+//     console.log("hello jee");
+// }
+// else if(number==4){
+//     console.log("namaste duniya");
+// }
+// else if(number==5){
+//     console.log("hellow akshat");
+// }
+// else{
+//     console.log("kuch nhi");
+// }
+
+let number=6;
+switch(number){
+    case 1:{
+        console.log("jai hoo");
+        break;
+    }
+    case 2:{
+        console.log("maja aa gya");
+        break;
+    }
+    case 3:{
+        console.log("hello jee");
+        break;
+    }
+    case 4:{
+        console.log("namaste duniya");
+        break;
+    }
+    case 5:{
+        console.log("hellow akshat");
+        break;
+    }
+    default:{
+        console.log("kuch nhi hai ");
+    }
+
+}
---
+++ operators.js
@@ -0,0 +1,68 @@
+            //  (arithmatic operator)
+
+// let number1=23;
+// let number2=22;
+// let number3=24;
+// console.log(number1+number2+number3);
+
+//  number1=23;
+//  number2=22;
+//   number3=24;
+// console.log(number1*number2*number3);
+
+                        //    (exponential)
+// let num1=2;
+// let num2=5;
+// let ans=(num1**num2);
+// console.log(ans);
+                    
+                            //  (   increment)
+//    let x=5;
+//    let y=20;
+ //    x++;
+//    console.log(x++);  
+//    console.log(x);     
+//    console.log(y++);     
+//    console.log(y);     
+
+                  //    (Assignment Operators)
+
+//    let name1="akshat patidar";  
+//    let number=20;
+//    console.log(name1);       //aksaht patidar
+//    console.log(number);      //20
+
+                        // (Comparison Operators)
+
+    // let x="10";
+    // let y=10;
+    // let z=true;
+    // console.log(typeof(x));           //string
+    // console.log(typeof(y));         //number
+    // console.log(typeof(z));           //boolean
+
+// let a=10;
+// let b=15;
+// let ans=(a==b);
+// console.log(ans);
+
+// let a="10";
+// let b=10;
+// // let ans=(a==b);
+// console.log(a==b);      //true        
+// console.log(a===b);      //flase    
+
+                        //(Bitwise Operators)
+  // 1.->  Bitwise AND (&)
+//    let num1 = 5;
+    // Binary representation of 5 : 0 1 0 1
+           
+    // let num2 = 7;
+    // Binary representation of 7 : 0 1 1 1
+                    
+//   console.log(num1 & num2);         // 5
+
+                            //    (Ternary Operators)
+
+ let myAge=21;
+ (myAge>=18)?(console.log("can drive")):(console.log("cannot drive"));
