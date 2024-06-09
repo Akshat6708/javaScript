3f631e602470d74a8333ed6554fb8e9503678167
Nitin bansiya <akshatpatidar29@gmail.com>
2024-06-09 11:14:15 +0530

    add day_7  folder

---
+++ day_7/deadZone.js
@@ -0,0 +1,50 @@
+
+
+    //        [ global scope]
+    
+// let age =21;
+// console.log(age);
+
+// {
+//     console.log(age);
+// }
+// for(let i=0 ;i<1;i++){
+//     console.log('loop ', age);
+// }
+
+// function greet(){
+//     console.log('function',  age);
+// }
+// greet();
+
+
+
+
+            //   [ function scope]
+
+// function sayName(){
+//     let lastName="patidar";
+//     console.log("akshat",lastName)
+// }
+// console.log(lastName);     //error
+// sayName();
+
+
+             // [deadzone]
+
+    console.log(marks)
+    console.log("akshat"); 
+    console.log("patidar");
+    let marks=100;
+    console.log(marks)         
+ //     line no. 35 to 38 called temprory dead zone
+
+
+
+
+
+
+
+
+
+
